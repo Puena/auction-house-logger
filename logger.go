9c86163e1ee492f62e1326177aa60845d598de9d
Platonov Dmitriy <woweuropeluffy@gmail.com>
2023-10-27 00:51:53 +0300

    add wrapper

--- logger.go
+++ logger.go
@@ -10,6 +10,50 @@ import (
 
 var Logger = globalLog.Output(os.Stdout)
 
+type LogWrapper struct {
+	log *zerolog.Logger
+}
+
+type LogWrapperEvent struct {
+	logEvent *zerolog.Event
+}
+
+func (l *LogWrapperEvent) Msg(msg string) *LogWrapperEvent {
+	l.logEvent.Msg(msg)
+	return l
+}
+
+func (l *LogWrapperEvent) Str(key string, val string) *LogWrapperEvent {
+	l.logEvent.Str(key, val)
+	return l
+}
+
+func (l *LogWrapperEvent) Int(key string, val int) *LogWrapperEvent {
+	l.logEvent.Int(key, val)
+	return l
+}
+
+func (l *LogWrapperEvent) Err(err error) *LogWrapperEvent {
+	l.logEvent.Err(err)
+	return l
+}
+
+func (l *LogWrapper) Info() *LogWrapperEvent {
+	return &LogWrapperEvent{logEvent: l.log.Info()}
+}
+
+func (l *LogWrapper) Debug() *LogWrapperEvent {
+	return &LogWrapperEvent{logEvent: l.log.Debug()}
+}
+
+func (l *LogWrapper) Error() *LogWrapperEvent {
+	return &LogWrapperEvent{logEvent: l.log.Error()}
+}
+
+func NewLogWrapper() *LogWrapper {
+	return &LogWrapper{log: &Logger}
+}
+
 type getTracingID func(context.Context) string
 
 type tracingIDHook struct {
